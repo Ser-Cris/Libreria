3107b6cb8ba0e229f18769d37e131d4eab15130f
artu-us <132608433+artu-us@users.noreply.github.com>
2025-02-20 19:32:34 -0600

    Update Libreria_DB.sql
    Ejecución inmediata

--- Libreria_DB.sql
+++ Libreria_DB.sql
@@ -1,14 +1,6 @@
 create database LibOnline;
 use LibOnline; 
 
-create table Logins(
-id_login INT auto_increment,
-correo VARCHAR(40) NOT NULL,
-contrasena VARCHAR(12) NOT NULL,
-privilegio VARCHAR(15) NOT NULL,
-UNIQUE KEY (correo),
-PRIMARY KEY (id_login)
-);
 
 create table Usuarios(
 id_usuario INT auto_increment,
@@ -16,12 +8,18 @@ id_direccion INT NOT NULL,
 id_login INT NOT NULL,
 nombre VARCHAR(30) NOT NULL,
 apellidos VARCHAR(30) NOT NULL,
-numero_telefonico NUMERIC(10) NOT NULL,
+numero_telefonico VARCHAR(10) NOT NULL,
 PRIMARY KEY(id_usuario)
 );
 
-ALTER TABLE Usuarios ADD FOREIGN KEY (id_direccion) REFERENCES Direcciones(id_direccion);
-ALTER TABLE Usuarios ADD FOREIGN KEY (id_login) REFERENCES Logins(id_login);
+create table Logins(
+id_login INT auto_increment,
+correo VARCHAR(40) NOT NULL,
+contrasena VARCHAR(256) NOT NULL,
+privilegio VARCHAR(15) NOT NULL,
+UNIQUE KEY (correo),
+PRIMARY KEY (id_login)
+);
 
 create table Direcciones(
 id_direccion INT auto_increment,
@@ -35,18 +33,7 @@ num_exterior NUMERIC(4) NOT NULL,
 PRIMARY KEY (id_direccion)
 );
 
-create table Compras (
-id_compra INT auto_increment,
-id_usuario INT NOT NULL,
-id_libro INT NOT NULL,
-estatus VARCHAR(20) NOT NULL,
-fecha_pedido DATE NOT NULL,
-total NUMERIC(6) NOT NULL,
-PRIMARY KEY (id_compra)
-);
 
-ALTER TABLE Compras ADD FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario);
-ALTER TABLE Compras ADD FOREIGN KEY (id_libro) REFERENCES Libros(id_libro);
 
 create table Envios(
 id_envio INT auto_increment,
@@ -56,9 +43,6 @@ fecha_entrega DATETIME NOT NULL,
 estatus VARCHAR(20) NOT NULL,
 PRIMARY KEY (id_envio)
 );
-
-ALTER TABLE Envios ADD FOREIGN KEY (id_compra) REFERENCES Compras(id_compra);
-ALTER TABLE Envios ADD FOREIGN KEY (id_usuario) REFERENCES Compra(id_usuario);
 
 create table Libros(
 id_libro VARCHAR(12) NOT NULL,
@@ -71,6 +55,34 @@ precio NUMERIC(5) NOT NULL,
 PRIMARY KEY (id_libro)
 );
 
+create table Compras (
+id_compra INT auto_increment,
+id_usuario INT NOT NULL,
+id_libro VARCHAR(12) NOT NULL,
+estatus VARCHAR(20) NOT NULL,
+fecha_pedido DATE NOT NULL,
+total NUMERIC(6) NOT NULL,
+PRIMARY KEY (id_compra),
+FOREIGN KEY (id_libro) REFERENCES Libros(id_libro)
+);
+
+
+
+ALTER TABLE Usuarios ADD FOREIGN KEY (id_direccion) REFERENCES Direcciones(id_direccion);
+ALTER TABLE Usuarios ADD FOREIGN KEY (id_login) REFERENCES Logins(id_login);
+ALTER TABLE Envios ADD FOREIGN KEY (id_compra) REFERENCES Compras(id_compra);
+ALTER TABLE Compras ADD FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario);
+/*ALTER TABLE Compras ADD FOREIGN KEY (id_libro) REFERENCES Libros(id_libro);*/
+ALTER TABLE Envios ADD FOREIGN KEY (id_usuario) REFERENCES Compras(id_usuario);
+
+
+
+/*
 show tables;
+insert into Logins (correo,contrasena,privilegio) values ('20223tn999@utez.edu.mx','Cisco123','cliente');
+select * from Logins;
+insert into clientes 
+  values ('Marcos Luis','marcosluis@gmail.com',aes_encrypt('5390700823285988','xyz123'));
 drop database libonline;
 drop table Compras;
+*/
